0904b91f113dff82d3f8a29177f76af21fa88988
shuvamx99 <shuvam.pandey@yugen.ai>
2024-05-04 11:35:05 +0530

    updated datagen

--- datagen/gen_fake_data.py
+++ datagen/gen_fake_data.py
@@ -5,6 +5,7 @@ from faker import Faker
 import random
 from uuid import uuid4
 from confluent_kafka import Producer
+from datetime import datetime
 
 fake = Faker()
 
@@ -40,8 +41,92 @@ def gen_user_data(num_user_records: int) -> None:
         
     return
 
+def random_user_agent():
+    return fake.user_agent()
+
+def random_ip():
+    return fake.ip_address()
+
+
+def generate_click_event(user_id, product=None):
+    click_id = str(uuid4())
+    product_id = product_id or str(uuid4())
+    product = fake.word()
+    price = fake.pyfloat(left_digits=2, right_digits=2, positive=True)
+    url = fake.uri()
+    user_agent = random_user_agent()
+    ip_address = random_ip()
+    datetime_occured = datetime.now()
+
+    click_event = {
+        "click_id": click_id,
+        "user_id": user_id,
+        "product_id": product_id,
+        "product": product,
+        "price": price,
+        "url": url,
+        "user_agent": user_agent,
+        "ip_address": ip_address,
+        "datetime_occured": datetime_occured.strftime("Y-%m-%d %H:%M:%S.%f")[-3]
+
+    }
+
+    return click_event
+
+
+def generate_checkout_event(user_id, product_id):
+    payment_method = fake.credit_card_provider()
+    total_amount = fake.pyfloat(left_digits=3, right_digits=2, positive=True)
+    shipping_address = fake.address()
+    billing_address = fake.address()
+    user_agent = random_user_agent()
+    ip_address = random_ip()
+    datetime_occured = datetime.now()
+
+    checkout_event = {
+        "checkout_id": str(uuid4()),
+        "user_id": user_id,
+        "product_id": product_id,
+        "payment_method": payment_method,
+        "total_amount": total_amount,
+        "shipping_address": shipping_address,
+        "billing_address": billing_address,
+        "user_agent": user_agent,
+        "ip_address": ip_address,
+        "datetime_occured": datetime_occured.strftime("%Y-%m-%d %H:%M:%S.%f")[
+            :-3
+        ],
+    }
+
+    return checkout_event
+
+
+def push_to_kafka(event, topic):
+    producer = Producer({
+        'bootstrap_servers': 'kafka:9092'
+    })
+    producer.produce(topic, json.dumps(event).encode('utf-8'))
+    producer.flush()
+
+
 def gen_clickstream_data():
-    return
+    for _ in range(num_click_records):
+        user_id = random.randint(1, 100)
+        click_event = generate_click_event(user_id)
+        push_to_kafka(click_event, 'clicks')
+
+        while random.randint(1, 100) >= 50:
+            click_event = generate_click_event(
+                user_id, click_event['product_id']
+            )
+            push_to_kafka(click_event, 'clicks')
+            push_to_kafka(
+                generate_checkout_event(
+                    click_event["user_id"], click_event["product_id"]
+                ),
+                "checkouts"
+            )
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
